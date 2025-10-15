f9ebabc6856ff8deb5e619113113d899e9738a93
Eren Sarialp <erensrlp@gmail.com>
2025-10-15 19:13:57 +0300

    enable feign clients

---
+++ product-service/src/main/java/com/turkcell/product_service/ProductsController.java
@@ -0,0 +1,16 @@
+package com.turkcell.product_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductsController {
+
+    @GetMapping
+    public String get()
+    {
+        return "Product Service";
+    }
+}
